e8b40cb31b49ec52aeebf7c20092a3c80c97aa3a
Far McKon <FarMcKon@gmail.com>
2013-03-08 16:44:13 -0500

    pep8 updates

--- pypeople/__main__.py
+++ pypeople/__main__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-from __future__ import ( unicode_literals, print_function, with_statement, absolute_import )
+from __future__ import (unicode_literals, print_function, with_statement, absolute_import)
 
 try:
     from . import cmdline
 except ValueError:
     import cmdline
+
 cmdline.main()
